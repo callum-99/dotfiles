dcffe26cddbf5c38e16c00100d66bc96efd6d2b4
Callum Yarnold <callum@yarnold.co.uk>
2025-05-31 15:59:09 +0100

    Elara: add new wsl machine

---
+++ hosts/wsl/elara/default.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, inputs, ... }: {
+  imports = [
+    ../../common/global
+    ../../common/global/linux.nix
+    ../../common/users/callum
+    ../../common/optional/ssh.nix
+  ];
+
+  # Set hostname
+  networking.hostName = "elara";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  # Titan-specific packages
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    neovim
+  ];
+
+  wsl = {
+    enable = true;
+    defaultUser = "callum";
+    startMenuLaunchers = false;
+    useWindowsDriver = true;
+    wslConf.interop.appendWindowsPath = false;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "25.05";
+}
+
