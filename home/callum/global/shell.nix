5a02bfa44f53bcc7f5c46722d99accff737d710f
Callum Yarnold <callum@yarnold.co.uk>
2025-06-20 11:44:38 +0100

    Home: Switch from oh-my-zsh to custom zsh config

--- home/callum/global/shell.nix
+++ home/callum/global/shell.nix
@@ -6,21 +6,59 @@
     enableCompletion = true;
     syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "docker" "kubectl" ];
-      theme = "robbyrussell";
-    };
-
     initContent = ''
       # Additional zsh configuration
       export PATH="$HOME/.local/bin:$PATH"
+
+      # Key bindings
+      bindkey "^[[3~" delete-char                   # Delete key
+      bindkey "^[[H" beginning-of-line              # Home key
+      bindkey "^[[F" end-of-line                    # End key
+      bindkey "^[[1;5C" forward-word                # Ctrl+Right arrow
+      bindkey "^[[1;5D" backward-word               # Ctrl+Left arrow
+      bindkey "^[[3;5~" delete-word                 # Ctrl+Delete
+      bindkey "^H" backward-delete-word             # Ctrl+Backspace
+
+      autoload -U up-line-or-beginning-search
+      autoload -U down-line-or-beginning-search
+      zle -N up-line-or-beginning-search
+      zle -N down-line-or-beginning-search
+      bindkey "^[[A" up-line-or-beginning-search   # Up arrorw
+      bindkey "^[OA" up-line-or-beginning-search   # Alt up arrow
+      bindkey "^[[B" down-line-or-beginning-search # Down arrow
+      bindkey "^[OB" down-line-or-beginning-search # Alt down arrow
     '';
 
     completionInit = ''
-      # Disable ls alias
-      zstyle ':omz:lib:directories' aliases no
+      # Init completion system
+      autoload -Uz compinit
+      compinit
+
+      # Case insensitive completion
+      zstyle ':completion:*' menu select
+
+      # Completion caching
+      zstyle ':completion:*' use-cache yes
+      zstyle ':completion:*' cache-path ~/.zsh/cache
+
+      # Better completion for kill command
+      zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+      # Don't complete uninteresting files
+      zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+      zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+      zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+      zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
     '';
+
+    history = {
+      size = 10000;
+      save = 10000;
+      share = true;
+      ignoreDups = true;
+      ignoreSpace = true;
+      extended = true;
+    };
   };
 
   # Configure starship prompt
@@ -33,8 +71,6 @@
         success_symbol = "[➜](bold green)";
         error_symbol = "[✗](bold red)";
       };
-
-      # Add more starship configuration here
     };
   };
 
