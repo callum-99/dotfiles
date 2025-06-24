84dd973a035c387fcc9b9616f3de38626285681f
Callum Yarnold <callum@yarnold.co.uk>
2025-06-24 19:02:13 +0100

    Phobos: Update to new monitor/workspace spec

--- home/callum/profiles/phobos.nix
+++ home/callum/profiles/phobos.nix
@@ -12,15 +12,46 @@
     zsh
   ];
 
-  # Monitor configuration
-  monitors = [
-    {
-      name = "eDP-1";
+  workspaces = {
+    "main" = {
+      id = 1;
+    };
+
+    "dev" = {
+      id = 2;
+      windowRules = [
+        { class = "^org.wezfurlong.wezterm$"; }
+        { class = "^Emacs$"; }
+        { class = "^code$"; }
+      ];
+    };
+
+    "web" = {
+      id = 3;
+      windowRules = [
+        { class = "^firefox$"; }
+      ];
+    };
+
+    "music" = {
+      id = 4;
+      windowRules = [
+        { class = "^feishin$"; }
+        { class = "^Spotify$"; }
+      ];
+    };
+  };
+
+  monitors = {
+    "eDP-1" = {
       width = 2560;
       height = 1600;
-      scale = 1.666667;
-    }
-  ];
+      scale = 1.667;
+      refreshRate = 60;
+      workspaces = [ "main" "dev" "web" "music" 5 6 7 8 9 ];
+      defaultWorkspace = "main";
+    };
+  };
 
   # SOPS configuration
   sops = {
