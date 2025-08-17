17f42e45755abeb99659224ef0bcd245e7f632f6
Callum Yarnold <git@yarnold.co.uk>
2025-08-17 19:34:12 +0100

    Add ssh hosts

--- home/users/callum/modules/ssh/default.nix
+++ home/users/callum/modules/ssh/default.nix
@@ -11,6 +11,38 @@ in {
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
+
+      includes = [ "extraHosts" ];
+
+      matchBlocks = {
+        "*" = {
+          setEnv = {
+            "TERM" = "xterm-256color";
+          };
+        };
+
+        horus = {
+          port = 1722;
+        };
+
+        anubis = {
+          port = 1722;
+        };
+
+        thunder1 = {
+          hostname = "100.64.0.8";
+        };
+
+        thunder2 = {
+          hostname = "100.64.0.9";
+        };
+
+        truenas = {
+          port = 1722;
+          user = "admin";
+          hostname = "100.64.0.6";
+        };
+      };
     };
   };
 }
