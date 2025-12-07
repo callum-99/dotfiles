adc5201cc6ac6acfa2755b90e9708f981d8dac7b
Callum Yarnold <git@yarnold.co.uk>
2025-12-07 18:29:51 +0000

    Remove useless stuff in neovim config

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -127,14 +127,7 @@ in {
 
       extraPlugins = with pkgs.vimPlugins; [
         nvim-web-devicons
-      ] ++ [
       ];
-
-      extraConfigLuaPre = ''
-        if vim.g.have_nerd_font then
-          require('nvim-web-devicons').setup {}
-        end
-      '';
 
       extraConfigLuaPost = ''
         -- vim: ts=2 sts=2 sw=2 et
